6c6f91f040cdb4cddfcb33a314d1ab240a9f5a4f
Vykstorm <victorruizgomezdev@gmail.com>
2018-11-16 11:45:25 +0100

    Added pyforms-gui dependency library

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         author = 'Vykstorm',
         author_email = 'victorruizgomezdev@gmail.com',
         python_requires = '>=2.7',
-        install_requires = ['numpy'],
-        dependency_links = [],
+        install_requires = ['numpy', 'pyforms-gui'],
+        dependency_links = ['https://github.com/Vykstorm/pyforms-gui/tarball/master#egg=pyforms-gui'],
         keywords = ['numpy', 'examples']
     )
